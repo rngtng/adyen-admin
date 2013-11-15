2c87574082fe88ec0ba188e42f41509a3e3e01f8
Tobias Bielohlawek <tobi@soundcloud.com>
2013-11-15 15:43:16 +0100

    less magic with setting the path, structure cleanup

--- lib/adyen-admin/skin.rb
+++ lib/adyen-admin/skin.rb
@@ -17,91 +17,99 @@ module Adyen
       attr_reader :code, :name, :path
 
       def initialize(attributes = {})
-        attributes.each do |key, value|
-          send("#{key}=", value)
+        if attributes[:path] && attributes[:code]
+          raise ArgumentError, "Either :path or :code has to be provided"
         end
 
-        self.path ||= File.expand_path File.join(Skin.default_path, [name,code].compact.join("-"))
-        self.path ||= File.expand_path File.join(Skin.default_path, name.to_s)
-        self.path ||= File.expand_path File.join(Skin.default_path, code.to_s)
-
-        raise ArgumentError, "No Code given" unless code
+        if new_path = attributes[:path]
+          raise ArgumentError, ":path '#{new_path}' is not a valid skin" unless Skin.is_skin_path?(new_path)
+          @path = new_path
+          @code = skin_data[:code] || code_from_path(path)
+          @name = skin_data[:name] || name_from_path(path)
+        else
+          @code = attributes[:code]
+          raise ArgumentError, ":code is not provided" unless code
+          @name = attributes[:name]
+          @path = Skin.path_from_code(code) || Skin.generate_path(code, name)
+        end
       end
 
-      def self.default_path
-        @default_path || "."
-      end
+      class << self
+        def default_path
+          @default_path || "."
+        end
 
-      def self.default_path=(default_path)
-        @default_path = default_path
-      end
+        def default_path=(default_path)
+          @default_path = default_path
+        end
 
-      # union remote and local skins. Local skins are frozen to
-      # indicate no availble remote counter part which avoid update
-      def self.all
-        @all ||= {}.tap do |hash|
-          if Adyen::Admin.authenticated?
-            self.all_remote.each do |skin|
+        # union remote and local skins. Local skins are frozen to
+        # indicate no availble remote counter part which avoid update
+        def all
+          @all ||= {}.tap do |hash|
+            if Adyen::Admin.authenticated?
+              all_remote.each do |skin|
+                hash[skin.code] = skin unless hash[skin.code]
+              end
+            end
+            all_local.each do |skin|
               hash[skin.code] = skin unless hash[skin.code]
             end
-          end
-          self.all_local.each do |skin|
-            hash[skin.code] = skin unless hash[skin.code]
-          end
-        end.values
-      end
-
-      # fetch all remote skins
-      def self.all_remote
-        page = Adyen::Admin.get(SKINS)
-        page.search(".data tbody tr").map do |node|
-          Skin.new({
-            :code => node.search("a")[0].content.strip,
-            :name => node.search("td")[1].content.strip,
-          })
+          end.values
         end
-      end
 
-      # fetch all local skins
-      def self.all_local
-        Dir[File.join(default_path.to_s, "*")].map do |skin_path|
-          Skin.new(:path => skin_path).freeze rescue nil
-        end.compact
-      end
+        # fetch all remote skins
+        def all_remote
+          page = Adyen::Admin.get(SKINS)
+          page.search(".data tbody tr").map do |node|
+            Skin.new({
+              :code => node.search("a")[0].content.strip,
+              :name => node.search("td")[1].content.strip,
+            })
+          end
+        end
 
-      # find a skin within remote + local ones
-      def self.find(skin_code)
-        self.all.select do |skin|
-          skin.code == skin_code
-        end.first
-      end
+        # fetch all local skins
+        def all_local
+          Dir[File.join(default_path.to_s, "*")].map do |skin_path|
+            Skin.new(:path => skin_path).freeze rescue nil
+          end.compact
+        end
 
-      def self.purge_cache
-        @all = nil
+        # find a skin within remote + local ones
+        def find(skin_code)
+          all.select do |skin|
+            skin.code == skin_code
+          end.first
+        end
+
+        def purge_cache
+          @all = nil
+        end
+
+        def is_skin_path?(skin_path)
+          %w(skin.yml inc css js).any? do |sub_path|
+            File.exists?(File.join(skin_path.to_s, sub_path))
+          end
+        end
+
+        def path_from_code(code)
+          Dir[File.join(Skin.default_path, "**/*#{code}*")].first
+        end
+
+        def generate_path(name, code)
+          dir_name = name.gsub(' ', '-').gsub(/[^a-z0-9-]/i, '')
+          File.expand_path File.join(Skin.default_path, [dir_name, code].compact.join("-"))
+        end
       end
 
       ##################################
 
-      def path=(new_path)
-        if Skin.is_skin_path?(new_path)
-          @path = new_path
-          if !skin_data.empty?
-            self.code = skin_data[:code]
-            self.name = skin_data[:name]
-          else
-            new_code, *new_name = File.basename(new_path).split("-").reverse
-            self.code ||= new_code
-            self.name ||= new_name.reverse.join("-")
-            raise ArgumentError if self.code && self.code != new_code
-          end
-        end
-      end
-
       def get_file(filename)
-        if self.path
-          File.join(self.path, filename).tap do |file|
+        if path
+          File.join(path, filename).tap do |file|
             if !File.exists?(file)
-              return File.join(File.dirname(self.path), parent_skin, filename)
+              return File.join(File.dirname(path), parent_skin, filename)
             end
           end
         end
@@ -164,7 +172,7 @@ module Adyen
 
       def decompile(filename, backup = true)
         # create backup of current, include any files
-        if self.path
+        if path
           if backup
             compress(/(zip|lock)$/, ".backup.zip")
           end
@@ -176,7 +184,7 @@ module Adyen
 
         Zip::File.open(filename) do |zip_file|
           zip_file.each do |file|
-            f_path = File.join(self.path || decompile_path, file.name.gsub("#{code}/", ""))
+            f_path = File.join(path || decompile_path, file.name.gsub("#{code}/", ""))
             FileUtils.mkdir_p(File.dirname(f_path))
             if File.directory?(f_path)
               `mkdir -p #{f_path}`
@@ -189,12 +197,12 @@ module Adyen
         self.path ||= decompile_path
 
         if backup
-          `mv .backup.zip #{File.join(self.path, ".backup.zip")}`
+          `mv .backup.zip #{File.join(path, ".backup.zip")}`
         end
       end
 
       def compile(output, pattern = /<!-- ### inc\/([a-z]+) -->(.+?)<!-- ### -->/m)
-        raise ArgumentError, "No Path given" unless self.path
+        raise ArgumentError, "No Path given" unless path
 
         output.scan(pattern) do |name, content|
           file = File.join(path, "inc/#{name}.txt")
@@ -233,7 +241,7 @@ module Adyen
 
       # http://stackoverflow.com/questions/3420587/ruby-mechanize-multipart-form-with-file-upload-to-a-mediawiki
       def upload
-        file = self.compress
+        file = compress
 
         page = Adyen::Admin.get(UPLOAD_SELECT)
         page = page.link_with(:href => Regexp.new(Regexp.escape(UPLOAD % code))).click
@@ -265,31 +273,24 @@ module Adyen
       end
 
       def to_s
-        self.code
+        code
       end
 
       def ==(skin)
-        self.code == skin.code
+        code == skin.code
       end
 
-      protected
-      def code=(c)
-        @code = c
+      private
+      def code_from_path(path)
+        File.basename(path).split("-").last
       end
 
-      def name=(n)
-        @name = n
+      def name_from_path(path)
+        File.basename(path).split("-")[0..-2].join('-')
       end
 
       def skin_data_file
         File.join(path, 'skin.yml')
-      end
-
-      def self.is_skin_path?(skin_path)
-        %w(skin.html.erb skin.yml inc css js).each do |sub_path|
-          return true if File.exists?(File.join(skin_path.to_s, sub_path))
-        end
-        false
       end
 
       def nested_subdirectory?(skin_path, file)
